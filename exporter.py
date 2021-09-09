fcdac549b0cfa3a1773abb5567cc8909fc069e9d
luaroncrew <ma-kir.ru@yandex.ru>
2021-09-09 13:30:49 +0200

    python2 mail exporter written

--- exporter.py
+++ exporter.py
@@ -1,7 +1,33 @@
-# this file will be sending all the data by mail
-
-import random
+import smtplib
 import os
+import logging
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+DESTINATION = os.getenv('DESTINATION')
+PASSWORD = os.getenv('EMAIL_PASSWORD')
+LOGIN = os.getenv('EMAIL_LOGIN')
+
+sent_from = LOGIN
+to = DESTINATION
+subject = 'OMG Super Important Message'
+body = 'YO ITS MAIL'
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.ehlo()
+server.login(LOGIN, PASSWORD)
+server.sendmail(sent_from, to, email_text)
+server.close()
+logging.info('mail sent')
 
 
