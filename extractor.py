fe3c232f45f93c31ba2baa090b4f536ef03d3239
luaroncrew <ma-kir.ru@yandex.ru>
2021-09-11 18:26:36 +0200

    requirements changed, example added

--- extractor.py
+++ extractor.py
@@ -1,4 +1,11 @@
-import PyPDF2
+from openpyxl import load_workbook
 
 
+wb = load_workbook('example.xlsx', data_only=True)
+sh = wb['Semaine 37 - 2021']
 
+print(sh['Z8'].value)
+
+
+def read_agenda(sheet):
+    pass
