5954232a6ae51187c86e3bbfc85f257b3bfdfdcb
luaroncrew <ma-kir.ru@yandex.ru>
2021-09-11 19:53:45 +0200

    event_extractor_main_funcs_started

--- extractor.py
+++ extractor.py
@@ -1,11 +1,32 @@
 from openpyxl import load_workbook
+import settings
 
+
+wednesday_letters = settings.SETTINGS['WEDNESDAY_LETTERS']
 
 wb = load_workbook('example.xlsx', data_only=True)
 sh = wb['Semaine 37 - 2021']
 
-print(sh['Z8'].value)
+
+def lesson_in_my_agenda(lesson):
+    content = lesson.split()
+    groups_to_exclude = ['STID-1-1', 'STID-1-11', 'STID-1-12']
+    for group in groups_to_exclude:
+        if (group in content) or (len(content) < 5):
+            return False
+    return True
 
 
 def read_agenda(sheet):
-    pass
+    ranges = sheet.merged_cells.ranges
+    for cell_range in ranges:
+        coordinate = str(cell_range).split(':')
+        content = sheet[coordinate[0]].value
+        # instead of using try for non-type cells
+        if content is None:
+            continue
+        if lesson_in_my_agenda(content):
+            print(content.split())
+
+
+read_agenda(sh)
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 # settings
 SETTINGS = {
     'EXTRACTING_FROM_PDF': False,
-    'DEBUG': True
+    'DEBUG': True,
+    'GROUP': 'STID-1-21',
+    'MONDAY_LETTERS': ['B', 'C', 'D', 'E'],
+    'WEDNESDAY_LETTERS': ['F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+
 }
