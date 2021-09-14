7a691cb00e9192a869f5888c214edc4983e9df2f
luaroncrew <ma-kir.ru@yandex.ru>
2021-09-14 08:41:59 +0200

    event extractor with sorting and time done

--- extractor.py
+++ extractor.py
@@ -6,18 +6,26 @@ wednesday_letters = settings.SETTINGS['WEDNESDAY_LETTERS']
 
 wb = load_workbook('example.xlsx', data_only=True)
 sh = wb['Semaine 37 - 2021']
+START_TIME = 8
 
 
 def lesson_in_my_agenda(lesson):
     content = lesson.split()
-    groups_to_exclude = ['STID-1-1', 'STID-1-11', 'STID-1-12']
+    groups_to_exclude = ['STID-1-1', 'STID-1-11', 'STID-1-12', 'STID-1-22']
     for group in groups_to_exclude:
-        if (group in content) or (len(content) < 5):
+        if (group in content) or (len(content) < 6):
             return False
     return True
 
 
+def stringify_time(time):
+    hours = str(int(time))
+    minutes = '30' if time % 1 != 0 else '00'
+    return hours + minutes + '00'
+
+
 def read_agenda(sheet):
+    needed_ranges = []
     ranges = sheet.merged_cells.ranges
     for cell_range in ranges:
         coordinate = str(cell_range).split(':')
@@ -26,7 +34,31 @@ def read_agenda(sheet):
         if content is None:
             continue
         if lesson_in_my_agenda(content):
-            print(content.split())
+            needed_ranges.append(cell_range)
+
+    for event_range in needed_ranges:
+        read_cell_indexes = str(event_range).split(':')
+        print(read_cell_indexes)
+        # reading start time cell index
+        start_time = str()
+        for element in read_cell_indexes[0]:
+            if element.isdigit():
+                start_time += element
+
+        # reading end time cell index
+        end_time = str()
+        for element in read_cell_indexes[1]:
+            if element.isdigit():
+                end_time += element
+
+        start_time = START_TIME + (int(start_time) - 8) / 2
+        end_time = START_TIME + (int(end_time) - 8) / 2 + 0.5
+        start_time = stringify_time(start_time)
+        end_time = stringify_time(end_time)
+        print(start_time, end_time)
+
+
+
 
 
 read_agenda(sh)
